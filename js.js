28af7ca274fa38ba65d790da39c61b9084f42c87
vandache <66990937+xShaken@users.noreply.github.com>
2024-03-07 03:04:14 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,41 @@
+
+const wrapper = document.querySelector(".form-structor");
+const btnPopup = document.querySelector(".loginbtn");
+const btnPopup1 = document.querySelector(".btn");
+
+
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+btnPopup1.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
+
+console.clear();
+
+const loginBtn = document.getElementById('login');
+const signupBtn = document.getElementById('signup');
+
+loginBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode.parentNode;
+	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			signupBtn.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
+
+signupBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode;
+	Array.from(e.target.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			loginBtn.parentNode.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
